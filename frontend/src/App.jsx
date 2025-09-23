781a80b026d3ddbcb94939571fc16d5c334c6f02
Samiksha <samikshachutkay@gmail.com>
2025-09-23 15:17:48 -0400

    made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { API_URL } from "./config/api";
 //const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
+console.log("API_URL at runtime =", API_URL);
 
 
 const tabs = [
